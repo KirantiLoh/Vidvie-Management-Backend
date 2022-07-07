3eda906a82ae80de870cf0cd77c1c6ae34adb5ff
Maurice Yang <mauriceyangmy28@gmail.com>
2022-07-07 10:02:07 +0700

    Adding datetimefilter and search fields in django admin

--- item/admin.py
+++ item/admin.py
@@ -4,11 +4,12 @@ from item.resources import ItemResource
 from .models import Item
 from django.contrib.admin.filters import AllValuesFieldListFilter
 from import_export.admin import ImportExportModelAdmin
+from rangefilter.filters import DateTimeRangeFilter
 
 # Register your models here.
 class ItemAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('name', 'condition', 'stock', 'date_added', 'date_updated')
-    list_filter= ['date_updated', ('condition', AllValuesFieldListFilter)]
+    list_filter= [('date_updated', DateTimeRangeFilter), ('condition', AllValuesFieldListFilter)]
     resource_class = ItemResource
     search_fields = ['name',]
 
