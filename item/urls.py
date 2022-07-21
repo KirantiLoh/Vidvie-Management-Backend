2c1137ac20b10a49b593274dc839561474a66da7
Maurice Yang <mauriceyangmy28@gmail.com>
2022-07-21 16:18:40 +0700

    Edited the path of stocks/divisions/[division] into stocks/division/[division] in item's urls.py

--- item/urls.py
+++ item/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('stocks/divisions/<slug:slug>', views.items_by_division_view, name = 'Items By Division'),
+    path('stocks/division/<slug:slug>', views.items_by_division_view, name = 'Items By Division'),
     path('stocks/<int:id>', views.item_view, name = 'Item View'),
 ]
